7a8d8814b5923d21bd52bf724b4ea0f104deb042
Hayk Ward <MyEmail>
2022-04-29 12:16:07 +0300

    commit lesson2

---
+++ HomeWork2.java
@@ -0,0 +1,70 @@
+package lesson2;
+import java.util.Arrays;
+
+public class HomeWork2 {
+
+    public static void main(String[] args) {
+
+/*
+    for (int d = 1; d <= 25; d++) {
+        System.out.println(d*d);
+    }
+*/
+/*
+    for (int d = 100; d < 1001; d++) {
+        if (d%3 == 0 && d%9 != 0) {
+            System.out.println(d);
+        }
+    }
+*/
+/*
+      int [][]  list_1 = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        int [][] list_2 = {
+                {1,2,3},
+                {4,5,6},
+                {7,8,9}
+        };
+
+        int[][] list_3 = new int[3][3];
+
+        if (list_1.length == list_2.length) {
+
+            for (int i = 0; i <= list_1.length-1; i++){
+                for (int j = 0; j<=list_1.length-1; j++) {
+                    list_3[i][j]=list_1[i][j]+list_2[i][j];
+                }
+            }
+            for (int l = 0; l<
+                    list_3.length; l++){
+                System.out.println(Arrays.toString(list_3[l]));
+            }
+        }else {
+            System.out.println("Матрицы не равны!");
+        }
+*/
+/*
+        int [] list = {24,7,56,9,17,0,60,35,8,11};
+        int a;
+        for (int i = 0; i<list.length-1;i++){
+            for (int j = 0; j<list.length-1;j++) {
+                if (list[j] > list[j + 1]) {
+                    a = list[j];
+                    list[j] = list[j + 1];
+                    list[j + 1] = a;
+                    a = list[j];
+                }
+            }
+        }
+        System.out.println(Arrays.toString(list));
+*/
+
+
+
+
+    }
+}
